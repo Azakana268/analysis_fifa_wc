00060456d91467e74483be0a30d45de806efbb00
Azakana268 <Benjaminac.biac@gmail.com>
2026-07-24 16:08:30 -0400

    Agrega pruebas y completa la documentacion del proyecto

--- tests/test_recent_data.R
+++ tests/test_recent_data.R
@@ -8,18 +8,28 @@ historicos <- read.csv(
 )
 combinados <- combinar_partidos(historicos, recientes)
 
-stopifnot(nrow(recientes) == 167L)
+stopifnot(nrow(recientes) == 168L)
 stopifnot(sum(recientes$year == 2022L) == 64L)
-stopifnot(sum(recientes$year == 2026L) == 103L)
-stopifnot(nrow(combinados) == nrow(historicos) + 167L)
+stopifnot(sum(recientes$year == 2026L) == 104L)
+stopifnot(nrow(combinados) == nrow(historicos) + 168L)
 stopifnot(!anyDuplicated(paste(
   combinados$date, combinados$home_team, combinados$away_team, sep = "|"
 )))
-stopifnot(max(recientes$date) == as.Date("2026-07-18"))
-stopifnot(!any(
+stopifnot(max(recientes$date) == as.Date("2026-07-19"))
+
+final <- recientes[
   recientes$date == as.Date("2026-07-19") &
     recientes$home_team == "Spain" &
-    recientes$away_team == "Argentina"
-))
+    recientes$away_team == "Argentina",
+  ,
+  drop = FALSE
+]
+stopifnot(nrow(final) == 1L)
+stopifnot(final$home_score[[1]] == 1L)
+stopifnot(final$away_score[[1]] == 0L)
+stopifnot(final$outcome[[1]] == "H")
+stopifnot(final$winning_team[[1]] == "Spain")
+stopifnot(final$losing_team[[1]] == "Argentina")
+stopifnot(grepl("extra time", final$win_conditions[[1]], fixed = TRUE))
 
-cat("OK: 167 partidos recientes validados e incorporados sin duplicados.\n")
+cat("OK: 168 partidos recientes, incluida la final 2026, validados sin duplicados.\n")
